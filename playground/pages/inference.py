4290b5b8af38a82945260b47cc65871b95c4874d
reoneo97 <reoneo97@gmail.com>
2022-04-11 11:18:27 +0800

    git changes

--- playground/pages/inference.py
+++ playground/pages/inference.py
@@ -1,3 +1,4 @@
+from re import S
 from this import d
 import streamlit as st
 import numpy as np
@@ -5,6 +6,9 @@ import plotly.express as px
 import plotly.figure_factory as ff
 from scipy.stats import norm
 from typing import List
+
+import pyro 
+import pyro.distributions as dist
 
 # st.header("Bayesian Inference")
 
@@ -17,6 +21,12 @@ N_SAMPLES = 250
 
 st.session_state["dist_1"] = None
 st.session_state["dist_2"] = None
+st.session_state["mu_1"] = None
+st.session_state["mu_2"] = None
+st.session_state["sd_1"] = None
+st.session_state["sd_2"] = None
+st.session_state["MCMC_Samples"] = None
+st.session_state["True_Samples"] = None
 
 def generate_dist():
     # Generate a simple bimodal distribution
@@ -37,13 +47,38 @@ def generate_dist():
         d1_samples = np.random.normal(m1, sd1, N_SAMPLES)
         d2_samples = np.random.normal(m2, sd2, N_SAMPLES)
         all_samples = np.concatenate([d1_samples, d2_samples])
-        st.write(all_samples.shape)
-        fig = ff.create_distplot([all_samples],["Posterior Distribution"],bin_size=0.4)
+        st.session_state["True_Samples"] = all_samples
+    
+    if st.session_state["True_Samples"] is not None:
+        fig = ff.create_distplot([st.session_state["True_Samples"]], 
+                                 ["Posterior Distribution"],
+                                 bin_size=0.4,show_rug=False)
         st.plotly_chart(fig)    
 
 def mcmc_section():
     st.subheader("Markov Chain Monte Carlo")
     st.markdown(mcmc_intro_txt)
+    start_pos = st.slider("MCMC Initial Position", -5., 5., 0.)
+    # Ideally checking session states should become a decorator function if its used frequently
+    if st.session_state["dist_1"]:
+        if st.button("Run MCMC") :
+
+            mcmc_samples = mcmc(start_pos)
+            st.session_state["MCMC_Samples"] = mcmc_samples
+        if st.session_state["MCMC_Samples"] is not None:
+            sample_length = len(st.session_state["MCMC_Samples"])
+            s_start, s_end = st.slider(
+                "Exclude Burn-in Period", 0, sample_length-1,
+                value=(sample_length//2, sample_length-1)
+            )
+            st.write(len(st.session_state["MCMC_Samples"]))
+            st.write(s_start, s_end)
+            mcmc_samples = st.session_state["MCMC_Samples"][s_start:s_end-1]
+            fig = ff.create_distplot([mcmc_samples], ["Posterior Distribution"],
+                                     bin_size=0.4, show_rug=False)
+            st.plotly_chart(fig)
+
+
     
     
 
@@ -66,20 +101,23 @@ mcmc_intro_txt = """
     
     """
 
-def pg3():
-    st.header("Bayesian Inference")
-    st.subheader("Generating Distribution")
-    generate_dist()
-    st.subheader("Variational Inference")
 
 def mixture_pdf(x:float) -> float: 
+    """Helper function to generate the pdf from the mixture distribution. Assumes
+    that the mixture weights are both 0.5
+
+    Args:
+        x (float): Position of the evaluated point
+
+    Returns:
+        float: Density of the point with mixture distribution
+    """
     return 0.5*st.session_state["dist_1"].pdf(x) + 0.5*st.session_state["dist_2"].pdf(x)
 
-def mcmc(start_pos: float, samples: int = 1000,) -> List[float]:
-    transitions = np.random.normal(size=1000)
-    u_s = np.random.uniform(size=1000)
-    start_pos = 0
-    samples = [start_pos]
+def mcmc(start_pos: float, samples: int = 5000,) -> List[float]:
+    transitions = np.random.normal(size=samples)
+    u_s = np.random.uniform(size=samples)
+    samples = []
     for t,u in zip(transitions,u_s):
         next_pos = start_pos + t
         pdf_ratio = mixture_pdf(next_pos)/mixture_pdf(start_pos)
@@ -88,4 +126,31 @@ def mcmc(start_pos: float, samples: int = 1000,) -> List[float]:
             start_pos = next_pos
         else:
             samples.append(start_pos)
+    return samples
+    
         
+
+def pg3():
+    st.header("Bayesian Inference")
+    st.subheader("Generating Distribution")
+    generate_dist()
+    st.subheader("Variational Inference")
+    mcmc_section()
+
+def mean_field1():
+    """
+    Initial Mean Field Inference by only using 1 Gaussian to approximate the 
+    posterior. Implemented using Pyro. 
+    """
+    def model(data):
+        # Ground Truth
+        for i in range(len(data)):
+            ber = pyro.sample(f"obs_{i}",dist.Bernoulli(0.5), obs=data[i])
+                    
+
+def mean_field2():
+    """
+    More advanced model to use 2 gaussians and a beta distribution to 
+    approximate the posterior
+    """
+    pass
